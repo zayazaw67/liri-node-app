bd5aaeadc04a54b79fc86618f7e77cff64933b35
Zaya <zayazaw67@gmail.com>
2019-10-01 16:41:56 -0500

    returns artist + track + album. user search input. added keys. required axios

--- keys.js
+++ keys.js
@@ -4,3 +4,11 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.omdbkey = {
+  omdbkey:process.env.omdbkey
+}
+
+exports.bandsintownkey = {
+  bandsintownkey:process.env.bandsintownkey
+}
--- liri.js
+++ liri.js
@@ -1,17 +1,24 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
-var spotifyKeys = require("./keys.js");
+var keys = require("./keys.js");
+const axios = require('axios');
+console.log(keys)
 
-console.log(spotifyKeys)
-var spotify = new Spotify({
-//   id: spotifyKeys.id,
-//   secret: spotifyKeys.secret
+var spotify = new Spotify(keys.spotify);
+var userInput = process.argv.slice(3).join(" ");
+var term = process.argv[2];
+
+if (term == "spotify-this-song") {
+
+spotify.search({ type: 'track', query: userInput }, function(err, data) { 
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  } else 
+  (
+  // combine these later, separate lines with \n
+  console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name)));  // logs the artist name
+  // console.log(data.tracks.items[0].name)  // logs the track name
+  // console.log(data.tracks.items[0].album.name)  // logs the album of the track
+  // console.log(data.tracks.items[0].preview_url)  // logs the preview link of the track
 });
- 
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// console.log(data); 
-// });
+}
