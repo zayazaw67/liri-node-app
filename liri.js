d53cff5f2501e3f2263697444017c8e2e6be3604
Zaya <zayazaw67@gmail.com>
2019-09-30 22:37:09 -0500

    installed npms

--- liri.js
+++ liri.js
@@ -1,1 +1,17 @@
 require("dotenv").config();
+var Spotify = require('node-spotify-api');
+var spotifyKeys = require("./keys.js");
+
+console.log(spotifyKeys)
+var spotify = new Spotify({
+//   id: spotifyKeys.id,
+//   secret: spotifyKeys.secret
+});
+ 
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+ 
+// console.log(data); 
+// });
