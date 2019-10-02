18788d8939fc37de9cfd194d2b32da9b723f5c9a
Zaya <zayazaw67@gmail.com>
2019-10-01 19:40:37 -0500

    working band and movie search

--- keys.js
+++ keys.js
@@ -1,14 +1,11 @@
-console.log('this is loaded');
-
+// console.log('this is loaded');
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
 exports.omdbkey = {
   omdbkey:process.env.omdbkey
 }
-
 exports.bandsintownkey = {
   bandsintownkey:process.env.bandsintownkey
 }
--- liri.js
+++ liri.js
@@ -2,23 +2,52 @@ require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
 const axios = require('axios');
-console.log(keys)
-
+var moment = require('moment');
 var spotify = new Spotify(keys.spotify);
 var userInput = process.argv.slice(3).join(" ");
 var term = process.argv[2];
 
 if (term == "spotify-this-song") {
-
-spotify.search({ type: 'track', query: userInput }, function(err, data) { 
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  } else 
-  (
-  // combine these later, separate lines with \n
-  console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name)));  // logs the artist name
-  // console.log(data.tracks.items[0].name)  // logs the track name
-  // console.log(data.tracks.items[0].album.name)  // logs the album of the track
-  // console.log(data.tracks.items[0].preview_url)  // logs the preview link of the track
-});
-}
+  spotify.search({ type: 'track', query: userInput }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    } else
+      (console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name +
+        " - " + data.tracks.items[0].name)));  // logs the artist name - track
+    console.log("This song is on album: " + data.tracks.items[0].album.name)  // logs the album of the track
+    console.log("Click the link to hear a preview of the track! " + data.tracks.items[0].preview_url)  // logs the preview link of the track
+  });
+} else if (term == "concert-this") {
+  axios.get('https://rest.bandsintown.com/artists/' + userInput + '/events?app_id=' + keys.bandsintownkey + '&date=upcoming')
+    .then(function (response) {
+      for (var i = 0; i < response.data.length; i++) {
+        console.log("Name: " + response.data[i].venue.name + "\nCity: " + response.data[i].venue.city + "\nCountry: " + response.data[i].venue.country + "\nTime: " + moment(response.data[i].datetime).format('MMMM Do YYYY, h:mm a')); // string version - all in one line doesn't look nice = console.log(JSON.stringify("Name: " + response.data[i].venue.name + " City: " + response.data[i].venue.city + " Country: " + response.data[i].venue.country + " Time: " + moment(response.data[i].datetime).format('MMMM Do YYYY, h:mm:ss a')));
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+} else if (term == "movie-this") {
+  if (userInput == true) {
+    axios.get('https://www.omdbapi.com/?t=' + userInput + '&apikey=trilogy') //+ keys.omdbkey)
+      .then(function (response) {
+        console.log("Title: " + response.data.Title + "\nDate released: " + response.data.Released + "\nProduced in: " + response.data.Country + "\nLanguages: " + response.data.Language
+          + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors);
+        // Can't add this inside above console.log. it comes up as [object Object] ???
+        console.log(response.data.Ratings[0]);
+        console.log(response.data.Ratings[1]);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  } else {
+  axios.get('https://www.omdbapi.com/?t=mr+nobody&apikey=trilogy')
+  .then(function (response) {
+    console.log(response)
+    // console.log("If you haven't watched 'Mr. Nobody,' then you should: " + response)
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  }
+} 
