360e3d84483e220e86a2ae3a5d5bd351c07726bb
Zaya <zayazaw67@gmail.com>
2019-10-02 13:07:02 -0500

    removed unneccessary comments, fixed readFile.

--- liri.js
+++ liri.js
@@ -8,75 +8,6 @@ var spotify = new Spotify(keys.spotify);
 var userInput = process.argv.slice(3).join(" ");
 var term = process.argv[2];
 
-// if (term == "spotify-this-song") {
-//   if (!userInput) {
-//     userInput = "lose yourself";
-//   }
-
-//   spotify.search({ type: 'track', query: userInput }, function (err, data) {
-//     if (err) {
-//       return console.log('Error occurred: ' + err);
-//     } else {
-//       console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name + " - " + data.tracks.items[0].name));  // logs the artist name - track
-//       console.log("This song is on album: " + data.tracks.items[0].album.name)  // logs the album of the track
-//       console.log("Click the link to hear a preview of the track! " + data.tracks.items[0].preview_url)  // logs the preview link of the track
-//     }
-//   });
-// }
-// else if (term == "concert-this") {
-// }
-// else if (term == "movie-this") {
-// }
-// else {
-//   axios.get('https://www.omdbapi.com/?t=mr+nobody&apikey=trilogy')
-//     .then(function (response) {
-//       console.log("If you haven't watched " + response.data.Title + " then you should: http://www.imdb.com/title/tt0485947/") // omdb api does not provide an imdb link to grab from response.data
-//     })
-//     .catch(function (error) {
-//       console.log(error);
-//     })
-//   } 
-// else if (term == "do-what-it-says") {
-//   fs.readFile("random.txt", "utf8", function (error, data) {
-//     if (error) { return console.log(error) };
-//     console.log(data);
-//     var newData = data.split(',')
-//     console.log(newData)
-//     // super wet. prob not the right way to do it
-//     spotify.search({ type: 'track', query: newData[1] }, function (err, data) {
-//       if (err) {
-//         return console.log('Error occurred: ' + err);
-//       } else {
-//         console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name + " - " + data.tracks.items[0].name));  // logs the artist name - track
-//         console.log("This song is on album: " + data.tracks.items[0].album.name)  // logs the album of the track
-//         console.log("Click the link to hear a preview of the track! " + data.tracks.items[0].preview_url)  // logs the preview link of the track
-//       }
-//     });
-//   })
-// }
-
-function switchBoard(command, query) {
-  switch (command) {
-    case "spotify-this-song":
-    case "song":
-    case "spotify":
-      // if (!query) {
-      //   query = "lose yourself"
-      // }
-      spotifySong(query);
-      break;
-    case "movie-this":
-      movieThis();
-      break;
-    case "concert-this":
-      concertThis();
-      break;
-    case "do-what-it-says":
-      readFile();
-      break;
-  }
-}
-
 function spotifySong(userInput) {
   if (!userInput) {
     // search defaults to lose yourself if nothing is entered
@@ -89,8 +20,9 @@ function spotifySong(userInput) {
       console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name + " - " + data.tracks.items[0].name));  // logs the artist name - track
       console.log("This song is on album: " + data.tracks.items[0].album.name)  // logs the album of the track
       if (data.tracks.items[0].preview_url === null) {
-      console.log("No preview found :(. Try another song.") } else {
-      console.log("Click the link to hear a preview of the track! " + data.tracks.items[0].preview_url)  // logs the preview link of the track
+        console.log("No preview found :(. Try another song.")
+      } else {
+        console.log("Click the link to hear a preview of the track! " + data.tracks.items[0].preview_url)  // logs the preview link of the track
       }
     }
   });
@@ -98,11 +30,12 @@ function spotifySong(userInput) {
 
 function movieThis(userInput) {
   if (!userInput) {
+    // defaults to mr nobody if no input is entered
     userInput = "Mr Nobody"
   } axios.get('https://www.omdbapi.com/?t=' + userInput + '&apikey=trilogy') //+ keys.omdbkey)
     .then(function (response) {
-      if (userInput === "Mr Nobody") {
-        console.log("If you haven't watched " + response.data.Title + " then you should: http://www.imdb.com/title/tt0485947/") // omdb api does not provide an imdb link to grab from response.data
+      if (userInput == "Mr Nobody") {
+        console.log("If you haven't watched " + response.data.Title + " then you should: http://www.imdb.com/title/tt0485947/" + "\nIt's on Netflix\n") // omdb api does not provide an imdb link to grab from response.data
       }
       console.log("Title: " + response.data.Title + "\nDate released: " + response.data.Released + "\nProduced in: " + response.data.Country + "\nLanguages: " + response.data.Language
         + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors);
@@ -128,11 +61,33 @@ function concertThis(userInput) {
   }
 }
 
-function readfile() {
-  //first read file
-  //parse for the command
-  //parse for the query
-  switchBoard(term, userInput)
+function readFile() {
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) { return console.log(error) };
+    var newData = data.split(',')
+    var term = newData[0];
+    var userInput = newData[1]
+    switchBoard(term, userInput)
+  });
+}
+
+function switchBoard(term, userInput) {
+  switch (term) {
+    case "spotify-this-song":
+    case "song":
+    case "spotify":
+      spotifySong(userInput);
+      break;
+    case "movie-this":
+      movieThis(userInput);
+      break;
+    case "concert-this":
+      concertThis(userInput);
+      break;
+    case "do-what-it-says":
+      readFile();
+      break;
+  }
 }
 
 switchBoard(term, userInput);
