19f71fb4248062533720b790aa861f854c584d36
Zaya <zayazaw67@gmail.com>
2019-10-01 19:55:59 -0500

    else statements and default results added

--- liri.js
+++ liri.js
@@ -8,6 +8,7 @@ var userInput = process.argv.slice(3).join(" ");
 var term = process.argv[2];
 
 if (term == "spotify-this-song") {
+  if (userInput == true) {
   spotify.search({ type: 'track', query: userInput }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
@@ -17,7 +18,15 @@ if (term == "spotify-this-song") {
     console.log("This song is on album: " + data.tracks.items[0].album.name)  // logs the album of the track
     console.log("Click the link to hear a preview of the track! " + data.tracks.items[0].preview_url)  // logs the preview link of the track
   });
-} else if (term == "concert-this") {
+} else {
+  spotify.search({ type: 'track', query: "the sign" }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    } else 
+    (console.log(datatracks.items[0].album.artists[0].name))
+  })
+}} else if (term == "concert-this") {
+  if (userInput == true) {
   axios.get('https://rest.bandsintown.com/artists/' + userInput + '/events?app_id=' + keys.bandsintownkey + '&date=upcoming')
     .then(function (response) {
       for (var i = 0; i < response.data.length; i++) {
@@ -27,6 +36,9 @@ if (term == "spotify-this-song") {
     .catch(function (error) {
       console.log(error);
     })
+  } else {
+    console.log("Search an artist or band!")
+  }
 } else if (term == "movie-this") {
   if (userInput == true) {
     axios.get('https://www.omdbapi.com/?t=' + userInput + '&apikey=trilogy') //+ keys.omdbkey)
@@ -43,8 +55,7 @@ if (term == "spotify-this-song") {
   } else {
   axios.get('https://www.omdbapi.com/?t=mr+nobody&apikey=trilogy')
   .then(function (response) {
-    console.log(response)
-    // console.log("If you haven't watched 'Mr. Nobody,' then you should: " + response)
+    console.log("If you haven't watched " + response.data.Title + " then you should: http://www.imdb.com/title/tt0485947/") // omdb api does not provide an imdb link to grab from response.data
   })
   .catch(function (error) {
     console.log(error);
