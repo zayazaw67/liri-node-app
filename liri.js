efe922e3664e41ef2f0c4bf54a2c25c8df6df454
Zaya <zayazaw67@gmail.com>
2019-10-02 10:09:22 -0500

    created function for spotify npm, do-what-it-says not as wet + dynamic. created switch. next step add rest of ifs to funcitons/switch

--- liri.js
+++ liri.js
@@ -9,28 +9,19 @@ var userInput = process.argv.slice(3).join(" ");
 var term = process.argv[2];
 
 if (term == "spotify-this-song") {
-  if (userInput) {
-    spotify.search({ type: 'track', query: userInput }, function (err, data) {
-      if (err) {
-        return console.log('Error occurred: ' + err);
-      } else {
-        console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name + " - " + data.tracks.items[0].name));  // logs the artist name - track
-        console.log("This song is on album: " + data.tracks.items[0].album.name)  // logs the album of the track
-        console.log("Click the link to hear a preview of the track! " + data.tracks.items[0].preview_url)  // logs the preview link of the track
-      }
-    });
+  if (!userInput) {
+    userInput = "lose yourself";
   }
-   else {
-      spotify.search({ type: 'track', query: "the sign" }, function (err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        } else {
-        console.log(data.tracks.items[5].album.artists[0].name + " - " + data.tracks.items[5].name);
-        console.log("This song is on album: " + data.tracks.items[5].album.name);
-        console.log("Click the link to hear a preview of the track! " + data.tracks.items[5].preview_url);
-        }
-      });
+
+  spotify.search({ type: 'track', query: userInput }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    } else {
+      console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name + " - " + data.tracks.items[0].name));  // logs the artist name - track
+      console.log("This song is on album: " + data.tracks.items[0].album.name)  // logs the album of the track
+      console.log("Click the link to hear a preview of the track! " + data.tracks.items[0].preview_url)  // logs the preview link of the track
     }
+  });
 }
 else if (term == "concert-this") {
   if (userInput) {
@@ -45,33 +36,36 @@ else if (term == "concert-this") {
   } else {
     console.log("Search an artist or band!")
   }
-} 
+}
 else if (term == "movie-this") {
   // less WET, but unsure how to console.log for no input
   if (!userInput) {
-    userInput = "Mr Nobody"    
+    userInput = "Mr Nobody"
   } axios.get('https://www.omdbapi.com/?t=' + userInput + '&apikey=trilogy') //+ keys.omdbkey)
-      .then(function (response) {
-        console.log("Title: " + response.data.Title + "\nDate released: " + response.data.Released + "\nProduced in: " + response.data.Country + "\nLanguages: " + response.data.Language
-          + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors);
-        console.log(response.data.Ratings[0]);
-        console.log(response.data.Ratings[1]);
-      }).catch(function (error) {
-        console.log(error);
-      })
-  } 
-  // else {
-  //   axios.get('https://www.omdbapi.com/?t=mr+nobody&apikey=trilogy')
-  //     .then(function (response) {
-  //       console.log("If you haven't watched " + response.data.Title + " then you should: http://www.imdb.com/title/tt0485947/") // omdb api does not provide an imdb link to grab from response.data
-  //     })
-  //     .catch(function (error) {
-  //       console.log(error);
-  //     })
-  //   } 
+    .then(function (response) {
+      if (userInput === "Mr Nobody") {
+        console.log("If you haven't watched " + response.data.Title + " then you should: http://www.imdb.com/title/tt0485947/") // omdb api does not provide an imdb link to grab from response.data
+      }
+      console.log("Title: " + response.data.Title + "\nDate released: " + response.data.Released + "\nProduced in: " + response.data.Country + "\nLanguages: " + response.data.Language
+        + "\nPlot: " + response.data.Plot + "\nActors: " + response.data.Actors);
+      console.log(response.data.Ratings[0]);
+      console.log(response.data.Ratings[1]);
+    }).catch(function (error) {
+      console.log(error);
+    })
+}
+// else {
+//   axios.get('https://www.omdbapi.com/?t=mr+nobody&apikey=trilogy')
+//     .then(function (response) {
+//       console.log("If you haven't watched " + response.data.Title + " then you should: http://www.imdb.com/title/tt0485947/") // omdb api does not provide an imdb link to grab from response.data
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     })
+//   } 
 else if (term == "do-what-it-says") {
-  fs.readFile("random.txt", "utf8", function(error, data){
-    if (error) {return console.log(error)};
+  fs.readFile("random.txt", "utf8", function (error, data) {
+    if (error) { return console.log(error) };
     console.log(data);
     var newData = data.split(',')
     console.log(newData)
@@ -85,13 +79,50 @@ else if (term == "do-what-it-says") {
         console.log("Click the link to hear a preview of the track! " + data.tracks.items[0].preview_url)  // logs the preview link of the track
       }
     });
-})
+  })
+}
+function switchBoard(command, query) {
+  switch (command) {
+    case "do-what-it-says":
+      readFile();
+      break;
+    case "spotify-this-song":
+    case "song":
+    case "spotify":
+      if (!query) {
+        query = "lose yourself"
+      }
+      spotifySong(query);
+      break;
+  }
 }
 
+function spotifySong(userInput) {
+  if (!userInput) {
+    userInput = "lose yourself";
+  }
+
+  spotify.search({ type: 'track', query: userInput }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    } else {
+      console.log(JSON.stringify(data.tracks.items[0].album.artists[0].name + " - " + data.tracks.items[0].name));  // logs the artist name - track
+      console.log("This song is on album: " + data.tracks.items[0].album.name)  // logs the album of the track
+      console.log("Click the link to hear a preview of the track! " + data.tracks.items[0].preview_url)  // logs the preview link of the track
+    }
+  });
+}
+
+function readfile() {
+  //first read file
+  //parse for the command
+  //parse for the query
+
+  switchBoard(term, userInput)
+}
+
+switchBoard(term, userInput);
 // convert ifs into functions. put into switch case to improve. gets rid of wet of do-what-it-says
-
-
-// !userInput won't work beacuse array is at index 5 for the sign. need offset. read documentation
 
 // function spotifyMe(song) {
 //   var spotify = new Spotify(keys.spotify);
